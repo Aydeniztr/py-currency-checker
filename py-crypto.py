351146476516eb0bbc24ea1055a9161c1ed37155
Aydeniztr <88689203+Aydeniztr@users.noreply.github.com>
2022-01-21 22:31:07 +0300

    Update py-crypto.py

--- py-crypto.py
+++ py-crypto.py
@@ -1,22 +1,39 @@
-import os
-import sys
-from bs4 import Beautifulsoup
-import requests
+#Description: This program gets the price of crypto currencies in real time
 
-coin = argv[1] 
+#Import the libraries 
+from bs4 import BeautifulSoup 
+import requests 
+import time
 
-def get_price(coin):
+#Create a function to get the price of a cryptocurrency
+def get_crypto_price(coin):
+#Get the URL
+    url = "https://www.google.com/search?q="+coin+"+price"
+    
+    #Make a request to the website
+    HTML = requests.get(url) 
   
-  url = str('https://www.google.com.tr/search?q='+ coin +'+price')
+    #Parse the HTML
+    soup = BeautifulSoup(HTML.text, 'html.parser') 
+  
+    #Find the current price 
+    #text = soup.find("div", attrs={'class':'BNeawe iBp4i AP7Wnd'}).text
+    text = soup.find("div", attrs={'class':'BNeawe iBp4i AP7Wnd'}).find("div", attrs={'class':'BNeawe iBp4i AP7Wnd'}).text
+#Return the text 
+    return text
 
-  data = requests.get(url)
-
-  dataparse = Beautifulsoup(HTML.text,'html.parser')
-	
-  # <div class="H0PQec">
-  # <div class="sbc esbc">
-	
-  text = dataparse.find(div,attr={'class':'H0PQec'}.find(div,attr={'class':'sbc esbc'}.text
-							 
-  return text
-							   
+#Create a main function to consistently show the price of the cryptocurrency
+def main():
+  #Set the last price to negative one
+  last_price = -1
+  #Create an infinite loop to continuously show the price
+  while True:
+    #Choose the cryptocurrency that you want to get the price of (e.g. bitcoin, litecoin)
+    crypto = 'bitcoin' 
+    #Get the price of the crypto currency
+    price = get_crypto_price(crypto)
+    #Check if the price changed
+    if price != last_price:
+      print(crypto+' price: ',price) #Print the price
+      last_price = price #Update the last price
+    time.sleep(3) #Suspend execution for 3 seconds.
